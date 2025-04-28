77730691d0871bef7a2f2463d2dda09c1658e5a7
synapticloop <contact@synapticloop.com>
2025-04-28 17:53:08 +1000

    removed unnecessary xmlns

--- scripts/model/Pencil.mjs
+++ scripts/model/Pencil.mjs
@@ -7,7 +7,6 @@ export class Pencil {
 	static WIDTH = 1200;
 	static HEIGHT = 300;
 	static SVG_START = `<svg xmlns="http://www.w3.org/2000/svg" ` +
-			`xmlns:xlink="http://www.w3.org/1999/xlink" ` +
 			`width="${Pencil.WIDTH}" ` +
 			`height="${Pencil.HEIGHT}"> ` +
 			`<pattern id="diagonalHatch" patternUnits="userSpaceOnUse" width="8" height="8">` +
