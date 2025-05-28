cbade25ea20fa2950065f59a0dadb37e606f1dcb
synapticloop <contact@synapticloop.com>
2025-05-28 13:58:16 +1000

    Updated accuracy definitions

--- scripts/model/meta/Accuracy.ts
+++ scripts/model/meta/Accuracy.ts
@@ -2,7 +2,8 @@ export class Accuracy {
 	private static ACCURACY_DESCRIPTION: Map<string, string[]> = new Map<string, string[]>([
 		[ "low" ,
 			[
-				"Physical pencil not present",
+				"Where any one of these things are true.",
+				"Physical pencil not present for measurement",
 				"Measurements of the pencil were not taken with a precision tool, and may have been estimated from supplied images.",
 				"The overall look of the pencil and the relative dimensions should be within reasonable and relative bounds.",
 				"The pencil may be based on a third party branded model that is identical to the original.",
@@ -12,7 +13,9 @@ export class Accuracy {
 		],
 		[ "medium" ,
 			[
-				"Physical pencil present, however no precision measurement tool was used, or physical pencil not present and image used.  Where an image is used for measurements, an accurate measuring scale is included with the image.",
+				"Where any one of these things are true.",
+				"Physical pencil present, however no precision measurement tool was used.",
+				"Physical pencil not present and image used for measurements, an accurate measuring scale is included with the image.",
 				"Not all pencil measurements were taken with a precision tool, (especially where there are internal components which access could not be gained)." ,
 				"External measurements may have been accurately measured, however internal components may not have been measured.",
 				"The pencil may be based on a first/third party branded model that is identical to the original.",
@@ -21,8 +24,8 @@ export class Accuracy {
 		],
 		[ "high",
 			[
-				"Physical pencil used for measurements." ,
-				"Physical pencil not present, but source technical documents used for measurements.",
+				"Where all of these things are true (with caveats).",
+				"Physical pencil used for measurements, or physical pencil not present, but source technical documents used for measurements.",
 				"Pencil measurements taken with a high precision tool." ,
 				"Internal measurements may not be available due to disassembly challenges.",
 				"The accuracy of the colours of the pencil's parts may not be reflected by the rendered images."
